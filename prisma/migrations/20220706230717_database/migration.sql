bafa06f80b73c9f06ca14577082d404dcb8f6b15
SharkmanTR <denizxman@hotmail.com>
2022-07-07 02:07:28 +0300

    master

--- prisma/migrations/20220706230717_database/migration.sql
+++ prisma/migrations/20220706230717_database/migration.sql
@@ -8,18 +8,10 @@ CREATE TABLE [dbo].[users] (
     [okulNo] INT NOT NULL,
     [email] NVARCHAR(1000) NOT NULL,
     [sifre] NVARCHAR(1000) NOT NULL,
+    [role] NVARCHAR(1000) NOT NULL CONSTRAINT [users_role_df] DEFAULT 'student',
     CONSTRAINT [users_pkey] PRIMARY KEY CLUSTERED ([id]),
     CONSTRAINT [users_okulNo_key] UNIQUE NONCLUSTERED ([okulNo]),
     CONSTRAINT [users_email_key] UNIQUE NONCLUSTERED ([email])
-);
-
--- CreateTable
-CREATE TABLE [dbo].[kirakitap] (
-    [id] INT NOT NULL IDENTITY(1,1),
-    [userId] INT NOT NULL,
-    [kitapId] INT NOT NULL,
-    [teslimTarih] NVARCHAR(1000) NOT NULL CONSTRAINT [kirakitap_teslimTarih_df] DEFAULT '',
-    CONSTRAINT [kirakitap_pkey] PRIMARY KEY CLUSTERED ([id])
 );
 
 -- CreateTable
@@ -42,13 +34,6 @@ CREATE TABLE [dbo].[kitaplar] (
     [sayfaSayisi] INT NOT NULL,
     [kategori] NVARCHAR(1000) NOT NULL,
     CONSTRAINT [kitaplar_pkey] PRIMARY KEY CLUSTERED ([id])
-);
-
--- CreateTable
-CREATE TABLE [dbo].[Kategoriler] (
-    [id] INT NOT NULL IDENTITY(1,1),
-    [kategoriIsmi] NVARCHAR(1000) NOT NULL,
-    CONSTRAINT [Kategoriler_pkey] PRIMARY KEY CLUSTERED ([id])
 );
 
 COMMIT TRAN;
